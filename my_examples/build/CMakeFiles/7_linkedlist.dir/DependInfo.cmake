bfb7d32587a73204bf08e3d7dd73afef92879af7
Marco Sterlini <marcosterlo@gmail.com>
2023-04-07 14:42:29 +0200

    Lezione del 30 marzo

---
+++ my_examples/build/CMakeFiles/7_linkedlist.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/marco/Desktop/Working directory/DM/my_examples/src/7_linkedlist.c" "CMakeFiles/7_linkedlist.dir/src/7_linkedlist.c.o" "gcc" "CMakeFiles/7_linkedlist.dir/src/7_linkedlist.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- my_examples/src/7_linkedlist.c
+++ my_examples/src/7_linkedlist.c
@@ -35,7 +35,7 @@ element_t *element_new(char *id) {
 
   // Id assignment
   e->id = malloc(strlen(id) + 1); // to reserve space to \0
-  strncpy(e->id, id, sizeof(id));
+  strncpy(e->id, id, strlen(id));
   // Little bit different from strcpy, strcpy doesn't require syze of source,
   // strncpy doesn't always add the string terminator if the string is already
   // full?
@@ -62,3 +62,197 @@ typedef struct {
 } list_t;
 
 // New list
+list_t *list_new(char *id) {
+  list_t *l = malloc(sizeof(list_t));
+  // Create first element
+  element_t *e = element_new(id);
+  // List initialization
+  l->first = e;
+  l->last = e;
+  l->length = 1;
+  return l;
+}
+
+// Append element at the end of my list
+void list_append_element(list_t *list, element_t *e) {
+  // Link the two elements
+  list->last->next = e;
+  e->prev = list->last; // Old last value
+  list->last = e;       // New last value
+  // Next of e set to null
+  e->next = NULL;
+  list->length++; // list length update
+}
+
+// Append a new element
+element_t *list_append_(list_t *list, char *id) {
+  element_t *e = element_new(id);
+  list_append_element(list, e);
+  return e;
+}
+
+// inserting an existing element after a given ID
+//
+//                             "after"
+//         ┌───┬──────┐     ┌───┬──────┐       ┌───┬──────┐
+// NULL ◄──┤PRV│      │◄────┤ P │      │     ┌─┤ P │      │
+//         ├───┘  ┌───┤     ├───┘  ┌───┤     │ ├───┘  ┌───┤
+//         │      │NXT├────►│      │ N ├──┐  │ │      │ N ├──► NULL
+//         └──────┴───┘     └────▲─┴───┘  │  │ └───▲──┴───┘
+//                               │   ┌───┐▼──▼──┐  │
+//                               └───┤ P │      │  │
+//                       new element ├───┘  ┌───┤  │
+//                    to be inserted │      │ N ├──┘
+//                                   └──────┴───┘
+//
+
+// Insert a new element after a certain ID
+void list_insert_element(list_t *list, element_t *new, char *after) {
+  element_t *e; // Temporary element
+  e = list->first;
+  // Iteration until the correct ID
+  do {
+    if (strcmp(e->id, after) == 0) {
+      new->next = e->next;
+      new->prev = e;
+      e->next->prev = new;
+      e->next = new;
+      break;
+    }
+  } while ((e = e->next));
+}
+
+// Add new element to my list
+element_t *list_insert(list_t *list, char *id, char *after) {
+  element_t *e = element_new(id);
+  list_insert_element(list, e, after);
+  return e;
+}
+
+// Delete one element from the list
+void list_delete_element(list_t *list, char *id) {
+  element_t *e; // Temporary element
+  e = list->first;
+  do {
+    if (strcmp(e->id, id) == 0) {
+      e->prev->next = e->next;
+      e->next->prev = e->prev;
+      element_free(e);
+      list->length--;
+      break;
+    }
+  } while ((e = e->next));
+}
+
+// Free the list
+void list_free(list_t *list) {
+  element_t *e, *next; // Temporary element
+  e = list->first;
+  do {
+    next = e->next;
+    list_delete_element(list, e->id);
+  } while ((e = next));
+  free(list);
+}
+
+//   _
+//  | |    ___   ___  _ __
+//  | |   / _ \ / _ \| '_ \ 
+//  | |__| (_) | (_) | |_) |
+//  |_____\___/ \___/| .__/
+//                   |_|
+
+typedef enum { ASCENDING = 0, DESCENDING, NUM_OF_LOOP_ORDERS } loop_order_t;
+
+// Function prototypes
+typedef void (*loop_fun_t)(element_t *e, loop_order_t o, void *userdata);
+//       (a) |----(b)----| |--------------------(c)--------------------|
+// a = the prototype return type
+// b = prototype type function
+// c = prototype function arguments
+
+// Doing so we create a prototype of a function that can be used for callbacks:
+// when we pass a function as an argument to another function when for example
+// we have to implement a similire task with little difference we could
+// differentiate trough this kind of functions
+
+// General function that depending on which loop_fun_t we pass will do different
+// things
+// Loop inside the list and execute fun on each element
+void list_loop(list_t *list, loop_fun_t fun, loop_order_t order,
+               void *userdata) {
+  element_t *e; // Tmp element
+
+  // First element according to order
+  if (order == ASCENDING)
+    e = list->first;
+  else
+    e = list->last;
+
+  // Loop
+  do {
+    // Execute fun on element e
+    fun(e, order, userdata);
+
+    // New value
+    if (order == ASCENDING)
+      e = e->next;
+    else
+      e = e->prev;
+
+  } while (e);
+}
+
+// Function implementation based on prototype
+void print_element(element_t *e, loop_order_t o, void *userdata) {
+  printf("%10s: %15p -> %15p\n", e->id, e->prev, e->next);
+}
+
+void print_element_with_index(element_t *e, loop_order_t o, void *userdata) {
+  // Explicit cast of userdata into size_t pointer, this is the iterator
+  size_t *i = (size_t *)userdata;
+  printf("list[%lu]: %s\n", (*i), e->id);
+  if (o == ASCENDING)
+    (*i)++;
+  else
+    (*i)--;
+}
+
+int main() {
+  char ids[4][10] = {"one", "two", "three", "four"};
+  element_t *e;
+  size_t i;
+  list_t *list = list_new("zero");
+  for (i = 0; i < 4; i++) {
+    e = element_new(ids[i]);
+    printf("Element %s is added!\n", ids[i]);
+    list_append_element(list, e);
+  }
+
+  // Loop ascending
+  i = 0;
+  list_loop(list, print_element_with_index, ASCENDING, &i);
+
+  // Loop descending
+  printf("Descending order\n");
+  i = list->length - 1;
+  list_loop(list, print_element_with_index, DESCENDING, &i);
+
+  // Print element informations
+  printf("Element info\n");
+  list_loop(list, print_element, ASCENDING, NULL);
+
+  // Remove one element
+  printf("Remove the two element\n");
+  list_delete_element(list, "two");
+
+  i = 0;
+  list_loop(list, print_element_with_index, ASCENDING, &i);
+
+  // Free the list
+  list_free(list);
+  // printf("List length: %lu\n", list->length); This will give segmentation
+  // fault error because the pointer list is freed
+
+  return 0;
+}
