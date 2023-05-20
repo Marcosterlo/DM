cee154fb40b7cf0a78aff517aaca9dd84ceb6533
Marco Sterlini <marcosterlo@gmail.com>
2023-05-20 18:31:57 +0200

    Exercise 1 fix

--- exercises/build/CMakeFiles/ex1.dir/src/exercise1.c.o.d
+++ exercises/build/CMakeFiles/ex1.dir/src/exercise1.c.o.d
@@ -1,19 +1,24 @@
 CMakeFiles/ex1.dir/src/exercise1.c.o: \
   /home/marco/Desktop/Working\ directory/DM/exercises/src/exercise1.c \
-  /usr/include/stdio.h \
-  /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
-  /usr/include/features.h /usr/include/features-time64.h \
+  /usr/include/ctype.h /usr/include/features.h \
+  /usr/include/features-time64.h \
   /usr/include/x86_64-linux-gnu/bits/wordsize.h \
   /usr/include/x86_64-linux-gnu/bits/timesize.h \
   /usr/include/stdc-predef.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
   /usr/include/x86_64-linux-gnu/bits/long-double.h \
   /usr/include/x86_64-linux-gnu/gnu/stubs.h \
   /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
-  /usr/lib/llvm-14/lib/clang/14.0.0/include/stddef.h \
-  /usr/lib/llvm-14/lib/clang/14.0.0/include/stdarg.h \
   /usr/include/x86_64-linux-gnu/bits/types.h \
   /usr/include/x86_64-linux-gnu/bits/typesizes.h \
   /usr/include/x86_64-linux-gnu/bits/time64.h \
+  /usr/include/x86_64-linux-gnu/bits/endian.h \
+  /usr/include/x86_64-linux-gnu/bits/endianness.h \
+  /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+  /usr/include/stdio.h \
+  /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+  /usr/lib/llvm-14/lib/clang/14.0.0/include/stddef.h \
+  /usr/lib/llvm-14/lib/clang/14.0.0/include/stdarg.h \
   /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
   /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
   /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
@@ -31,8 +36,6 @@ CMakeFiles/ex1.dir/src/exercise1.c.o: \
   /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
   /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
   /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
-  /usr/include/x86_64-linux-gnu/bits/endian.h \
-  /usr/include/x86_64-linux-gnu/bits/endianness.h \
   /usr/include/x86_64-linux-gnu/bits/byteswap.h \
   /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
   /usr/include/x86_64-linux-gnu/sys/select.h \
@@ -49,7 +52,4 @@ CMakeFiles/ex1.dir/src/exercise1.c.o: \
   /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h \
   /usr/include/alloca.h \
   /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
-  /usr/include/string.h \
-  /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
-  /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
-  /usr/include/strings.h /usr/include/ctype.h
+  /usr/include/string.h /usr/include/strings.h
--- exercises/src/exercise1.c
+++ exercises/src/exercise1.c
@@ -1,86 +1,139 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 #define MAX_LENGTH 300
 
 int main(int argc, char *argv[]) {
-  FILE *old = NULL, *new = NULL;
-  char *name = NULL, *new_name = NULL, *tmp = NULL;
+  FILE *old = NULL;
+  // FILE *new = NULL;
+  char *name = NULL, *tmp = NULL;
+  // char *new_name = NULL;
   char last = 0;
   size_t n = 0;
   ssize_t nread = 0;
-  int incr = 0, n_actual = 0, n_lines = 0;
+  int incr = 0, n_actual = 0, n_lines = 0, n_digits = 1, exit = 0;
   char *buffer = malloc(MAX_LENGTH);
 
-  if (argc != 4) {
-    printf("Exactly 3 argument must be passed: input gcode file name, output gcode file name and increment\n");
-    return 1;
-  } else {
-    printf("Passed input name file: %s\n", argv[1]);
-    name = argv[1];
-    printf("Passed output name file: %s\n", argv[2]);
-    new_name = argv[2];
-    printf("Passed increment: %d\n", atoi(argv[3]));
-    incr = atoi(argv[3]);
-  }
-  
-  old = fopen(name, "r");
-  new = fopen(new_name, "w");
-
-  if (!old) {
-    printf("File not opened correctly\n");
-    goto fail;
-  }
-  else if (!new) {
-    printf("New file not opened correctly\n");
-    goto fail;
-  }
-  else {
-    printf("File aperti correttamente\n");
-  }
-  
-  // Count number of lines
+  // Version with file output
   /*
-  while ((nread = getline(&buffer, &n, old)) != -1) {
-    if (!strcmp(buffer, "\n")) {
-      continue;
-    }
-    n_lines++;
+  if (argc != 4) {
+    printf("Exactly 3 argument must be passed: input gcode file name, output
+  gcode file name and increment\n"); return 1; } else { printf("Passed input
+  name file: %s\n", argv[1]); name = argv[1]; printf("Passed output name file:
+  %s\n", argv[2]); new_name = argv[2]; printf("Passed increment: %d\n",
+  atoi(argv[3])); incr = atoi(argv[3]);
   }
   */
 
+  if (argc != 3) {
+    printf("Exactly 2 argument must be passed: input gcode file name and "
+           "increment\n");
+    return 1;
+  } else {
+    fprintf(stderr, "Passed input name file: %s\n", argv[1]);
+    name = argv[1];
+    fprintf(stderr, "Passed increment: %d\n", atoi(argv[2]));
+    incr = atoi(argv[2]);
+  }
+
+  old = fopen(name, "r");
+  // new = fopen(new_name, "w");
+
+  if (!old) {
+    fprintf(stderr, "File not opened correctly\n");
+    goto fail;
+  }
+  // else if (!new) {
+  //   printf("New file not opened correctly\n");
+  //   goto fail;
+  // }
+  else {
+    fprintf(stderr, "File aperti correttamente\n");
+  }
+
+  // Count number of lines
+  while ((nread = getline(&buffer, &n, old)) != -1) {
+    // Flag ripristinata ogni volta che si legge una nuova riga
+    exit = 1;
+    // Ciclo for che continua finché non vengono esauriti i caratteri del buffer
+    // o viene disattivata la variabile flag
+    for (int i = 0; exit && i < nread; i++) {
+      // Se viene trovato un carattere diverso da un whitespace la variabile
+      // flag viene spenta e aumenta il numero di righe valido
+      if (!isspace(buffer[i])) {
+        exit = 0;
+        n_lines++;
+      }
+    }
+  }
+  // Comando per riportare il file pointer all'inizio
+  rewind(old);
+
+  // Algorithm to determine number of digits
+  // Numero effettivo di righe deve essere moltiplicato per l'incremento passato
+  n_lines = n_lines * incr;
+  // Se il numero di righe è minore di 10 si passa n_digits rimane inizializzata
+  // a 1
+  if (n_lines >= 10) {
+    // altrimenti si divide per 10, finché non otteniamo un numero minore di 10
+    // vuol dire che il numero è composto da un altro digit
+    while ((n_lines /= 10) >= 10) {
+      n_digits++;
+    }
+    // Ultima aggiunta per fare appattare tutto
+    n_digits++;
+  }
+
   // Parsing
   while ((nread = getline(&buffer, &n, old)) != -1) {
-    if (!strcmp(buffer, "\n")) {
+    // Modo simile a prima per escludere le righe che non siano solo whitespace
+    exit = 1;
+    for (int i = 0; exit && i < nread; i++) {
+      if (!isspace(buffer[i])) {
+        exit = 0;
+      }
+    }
+    if (exit) {
+      // Si passa alla prossima righa se il flag è ancora attivo, questo
+      // significa che la riga è solo composta da whitespaces
       continue;
     }
+    // In questo modo mi assicuro che tutte le righe finiscano con uno \n,
+    // solitamente l'unica ad avere questa caratteristica potrebbe essere
+    // l'ultima, uso la variabile flag last
     if (buffer[strlen(buffer) - 1] != '\n') {
       last = 1;
     }
+    // Printo all'inizio della riga N seguito da 0 quanti i digit calcolati e
+    // col numero attuale della riga
+    fprintf(stdout, "N%0*d", n_digits, n_actual);
+    n_actual += incr;
     while ((tmp = strsep(&buffer, " "))) {
+      // Se la riga ha spazi iniziali tmp è una stringa vuota: "" = '\0', devo
+      // fare il check che quindi il primo carattere del comando non sia vuoto o
+      // uguale a N perché già printato
       tmp[0] = toupper(tmp[0]);
-      switch (tmp[0])
-      {
-      case 'N':
-        fprintf(new, "N%0*d", 4, n_actual);
-        n_actual += incr;
-        break;
-      default:
-        fprintf(new, " %s", tmp);
-        break;
+      // Porto a maiuscola ogni eventuale carattere di comando
+      if (tmp[0] != '\0' && tmp[0] != 'N') {
+        // stampo il valore della stringa tmp preceduta da uno spazio di
+        // separazione
+        fprintf(stdout, " %s", tmp);
       }
     }
+    // Se la riga non presenta un newline lo aggiungo
     if (last) {
-      fprintf(new, "\n");
+      fprintf(stdout, "\n");
     }
   }
 
 fail:
+  // Libero la memoria dinamica allocata dalla stringa
   free(buffer);
-  if (new)
-    fclose(new);
+  // if (new)
+  //   fclose(new);
+  // chiudo il file
   if (old)
     fclose(old);
   return 0;
